137bb67611d08cf50d75116e7bb4b0565890b55f
Adam Lei <billparker149@gmail.com>
2020-02-08 21:20:46 -0800

    Initial Commit

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,16 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Adam on 2/8/20.
+//  Copyright © 2020 Adam. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+}
